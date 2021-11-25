699b18a33ee502dcfb95bc503a45f61d1dd49410
awawrzonkowska <syberytffc@gmail.com>
2021-11-25 09:35:50 +0100

    First code kata

---
+++ ListFilterer/ListFilterer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CodeKata.ListFiltering
+{
+    public static class ListFilterer
+    {
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            List<int> integers = new List<int>();
+
+            foreach (object e in listOfItems)
+            {
+                if (e is int)
+                {
+                    integers.Add((int)e);
+                }
+            }
+            return integers;
+        }
+    }
+}

ba5fa603fdcd4492be0ac0fb72d1d768bdd3b480
Alicja Wawrzonkowska <syberytffc@gmail.com>
2021-11-27 15:54:58 +0100

    CategorizeMembers using foreach

---
+++ CategorizeNewMembers/CategorizeMembers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeKata.CategorizeNewMembers
+{
+    public class CategorizeMembers
+    {
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            List<string> result = new List<string>();
+
+            foreach (int[] element in data)
+            {
+                result.Add(IsSenior(element));
+            }
+
+            return result;
+        }
+
+        private static string IsSenior(int[] member)
+        {
+            if (member[0] >= 55 && member[1] > 7)
+                return "Senior";
+            return "Open";
+        }
+    }
+}
